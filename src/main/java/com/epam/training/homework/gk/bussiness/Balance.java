1326dd577d97843b6ff19bde34c22231ec8f7347
Krisztian Gulyas <gulyas.krisztian1o9@gmail.com>
2017-12-17 13:50:50 +0100

    .keep packages

--- src/main/java/com/epam/training/homework/gk/bussiness/Balance.java
+++ src/main/java/com/epam/training/homework/gk/bussiness/Balance.java
@@ -4,9 +4,9 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface Balance {
-	void increase(BigDecimal value);
+	void increase(BigDecimal value, String reason);
 
-	void decrease(BigDecimal value);
+	void decrease(BigDecimal value, String reason);
 
 	List<Balance> getHistory();
 
